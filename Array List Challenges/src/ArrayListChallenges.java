f790d4dcc6246f6cc2149108c23c0b2227485f65
Jackson.Paul <Jackson.Paul@MU-C1-12.mullen.pvt.k12.co.us>
2017-09-13 14:56:21 -0600

    finished challenges 2 and 3

--- Array List Challenges/src/ArrayListChallenges.java
+++ Array List Challenges/src/ArrayListChallenges.java
@@ -1,11 +1,67 @@
-
+import java.util.*;
 public class ArrayListChallenges
 	{
-
+		static ArrayList myArray = new ArrayList <Double>();
+		static double temp;
 		public static void main(String[] args)
 			{
+				
+			challenge2();
+			challenge3();
+			challenge4();
+			challenge5();
+			challenge6();
 			
+			}
 
+		private static void challenge2()
+			{
+			myArray.add(98.3);
+			myArray.add(96.9);
+			myArray.add(100.3);
+			myArray.add(97.8);
+			myArray.add(98.9);
+			myArray.add(98.6);
+			myArray.add(101.7);
+			myArray.add(102.5);
+			myArray.add(97.6);
+			myArray.add(98.5);
+			}
+
+		private static void challenge3()
+			{
+			System.out.println("The fevers are: ");
+			for (int i = 0; i < myArray.size(); i++)
+				{
+				temp = (Double) myArray.get(i);
+				if (temp > 98.6)
+					{
+					System.out.println(temp);
+					}
+				}
+			System.out.println("");
+			}
+
+		private static void challenge4()
+			{
+			System.out.println("The temperatures in Celsius are: ");
+			for (int i = 0; i < myArray.size(); i++)
+				{
+				
+				}
+				
+			}
+
+		private static void challenge5()
+			{
+			
+				
+			}
+
+		private static void challenge6()
+			{
+			
+				
 			}
 
 	}
